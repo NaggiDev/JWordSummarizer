a5a79e220aea135d11bf3c170a1217469fcd3686
N <cloenn02@gmail.com>
2023-07-04 12:05:05 +0700

    Initial commit

---
+++ src/main/java/nagidev/jwordsummarizer/JWordSummarizerApplication.java
@@ -0,0 +1,13 @@
+package nagidev.jwordsummarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JWordSummarizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JWordSummarizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nagidev/jwordsummarizer/JWordSummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package nagidev.jwordsummarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JWordSummarizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
